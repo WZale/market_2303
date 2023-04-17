bb99d3d70d7192e002334da155ca64bec1040c7c
William Zale <williamzale@gmail.com>
2023-04-17 13:36:13 -0400

    Add forgotten 'expect vendor to be a Vendor' line to test

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -5,6 +5,7 @@ require './lib/vendor'
 RSpec.describe Vendor do
   it 'can initialize with readable attributes' do
     vendor = Vendor.new("Rocky Mountain Fresh")
+    expect(vendor).to be_a(Vendor)
     
     expect(vendor.name).to eq("Rocky Mountain Fresh")
     expect(vendor.inventory).to eq({})
