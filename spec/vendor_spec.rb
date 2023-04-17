8fa3cd9eb1f98916694ef866ebca35b31b934d8f
William Zale <williamzale@gmail.com>
2023-04-17 11:58:32 -0400

    Add test for check_stock method

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Vendor do
     expect(vendor.name).to eq("Rocky Mountain Fresh")
     expect(vendor.inventory).to eq({})
   end
+
+  it 'has a check_stock method' do
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    expect(vendor.check_stock(item1)).to eq(0)
+  end
 end
