646472123cc4000925c0bd0cd7f32dfa4de2643e
William Zale <williamzale@gmail.com>
2023-04-17 13:35:47 -0400

    Add forgotten 'expect item to be an Item' line to test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Item do
   it 'can initialize with readable attributes' do
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    expect(item1).to be_a(Item)
 
     expect(item2.name).to eq("Tomato")
     expect(item2.price).to eq(0.5)
