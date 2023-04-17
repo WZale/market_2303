862e99b0bce063c917b715d5366efcf5e3b8eb88
William Zale <williamzale@gmail.com>
2023-04-17 13:37:43 -0400

    Add 'it has an add_vendor method' test

---
+++ spec/market_spec.rb
@@ -0,0 +1,38 @@
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+RSpec.describe Market do
+  before(:each) do
+    @market = Market.new("South Pearl Street Farmers Market")
+    expect(@market).to be_a(Market)
+    
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+  end
+
+  it 'can initialize with readable attributes' do
+    expect(@market.name).to eq("South Pearl Street Farmers Market")
+    expect(@market.vendors).to eq([])
+
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+
+    @vendor3.stock(@item1, 65)
+  end
+
+  it 'has an add_vendor method' do
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+  end
+end
